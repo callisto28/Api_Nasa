810bb21a5627130bd008ca6a18c90e45db3ec842
callisto28 <iotaseb@gmail.com>
2021-09-13 12:39:59 +0200

    ADD[localDate}ADD ajout de la date de l'image

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-
+// require('dotenv').config();
 const nasa1 = document.querySelector(".nasa1");
 const explication = document.querySelector(".detail");
 const imageTexte = document.querySelector(".imgText");
 const image = document.querySelector(".img");
 
 
+
 const api = "wVFgJbKMy3w572mhbYv39N9r1YgW1zfCaDUClwOO";
+// const api = process.env.API_KEY;
 
 let day = new Date()
+let daytoday = day.toLocaleDateString()
+
+
+
 
 let dataDay = [];
 
@@ -15,9 +21,9 @@ async function getNasa() {
 
     let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api}`);
     let data = await response.json();
-    console.log(data);
+    // console.log(data);
     dataDay = data;
-    nasa1.innerText = dataDay.date.split();
+    nasa1.textContent = `Picture of the day (${daytoday})`;
     explication.textContent = dataDay.explanation;
     imageTexte.textContent = dataDay.title;
     image.src = dataDay.url;
